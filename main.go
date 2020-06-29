5ad3dd4b575e305060133636dd08a3901fc1ceb5
dinup-pillai <dinup.pillai@in.ibm.com>
2020-06-29 23:25:30 +0530

    minor changes

--- main.go
+++ main.go
@@ -7,29 +7,54 @@ import (
 	"runtime"
 	"strconv"
 	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	//log.SetFormatter(&log.JSONFormatter{})
-	var file, err = os.OpenFile("logs.txt", os.O_RDWR|os.O_CREATE, 0666)
+	log.SetLevel(log.DebugLevel)
+
+	logFilePath := "logs.txt"
+	noOfGoRoutines := 10
+
+	if len(os.Args) > 1 {
+		logFilePath = os.Args[1]
+	}
+
+	if len(os.Args) > 2 {
+		noOfGoRoutines, _ = strconv.Atoi(os.Args[2])
+	}
+
+	var file, err = os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("Could Not Open Log File : " + err.Error())
 	}
 	log.SetOutput(file)
 
 	var wg sync.WaitGroup
-	for i := 0; i < 50; i++ {
+	for i := 0; i < noOfGoRoutines; i++ {
 		wg.Add(1)
 		go func(wg *sync.WaitGroup, identifier int) {
 			defer wg.Done()
-			log.WithFields(
-				log.Fields{
-					"goid":       getGID(),
-					"identifier": identifier,
-				},
-			).Info("Something happened")
+			for {
+				log.WithFields(
+					log.Fields{
+						"goid":       getGID(),
+						"identifier": identifier,
+					},
+				).Info("Something happened")
+
+				log.WithFields(
+					log.Fields{
+						"goid":       getGID(),
+						"identifier": identifier,
+					},
+				).Debug("Debugging something")
+
+				time.Sleep(1 * time.Second)
+			}
 		}(&wg, i)
 	}
 
